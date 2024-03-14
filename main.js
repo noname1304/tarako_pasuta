895dbce173de323e0bebcf4452f58ea1ae8d67e5
noname1304 <115709660+noname1304@users.noreply.github.com>
2024-03-14 19:44:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ function text_replace() {
 let text_value = before_text.value;
     
 //テキストを変換 ..②
-text_value = text_value.replace(/りんご/g,"あおりんご");
+text_value = text_value.replace(/な/g,"にゃ");
+text_value = text_value.replace(/ね/g,"にぇ");
+text_value = text_value.replace(/na/g,"nya");
+text_value = text_value.replace(/Na/g,"Nya");
+text_value = text_value.replace(/NA/g,"NYA");
+text_value = text_value.replace(/ne/g,"nie");
+text_value = text_value.replace(/Ne/g,"Nie");
+text_value = text_value.replace(/NE/g,"NIE");
     
 //変換後のテキストエリアに表示 ..③
 after_text.value = text_value;
